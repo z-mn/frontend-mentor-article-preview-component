845959b833083ff0236acde81176929d2c6e563b
z-mn <155245405+z-mn@users.noreply.github.com>
2024-08-16 20:25:39 +0100

    mobile design complete

--- main.js
+++ main.js
@@ -2,22 +2,35 @@
 
 const shareButton = document.getElementById('button');
 const shareImage = document.getElementById('image');
-const profileSection = document.getElementById('profile')
 
 shareButton.addEventListener('mouseover', () => {
-    shareImage.style = 'fill: #FFFFFF; transition: 200ms fill ease-in';
+    shareImage.classList.add('about-section__swap-image--secondary');
     shareButton.style.cursor = 'pointer';
 })
 
 shareButton.addEventListener('mouseleave', () => {
-    shareImage.style = 'fill: #6E8098; transition: 200ms fill ease-out'; // this is stopping the button click image colour change.
+    shareImage.classList.remove('about-section__swap-image--secondary');
 })
 
 // Button click - UI change
 
+const aboutSection = document.getElementById('about');
+const marginText = document.getElementById('text');
+const profileSection = document.getElementById('profile');
+const profileContainer = document.querySelector('.profile__text-container');
+const profileImage = document.querySelector('.profile__image');
+const secondaryElements = document.getElementsByClassName('profle__element--secondary');
+
 shareButton.addEventListener('click', () => {
     shareButton.classList.toggle('about-section__swap-button--secondary');
     shareImage.classList.toggle('image--secondary');
+    aboutSection.classList.toggle('card__about-section--secondary');
+    marginText.classList.toggle('text__margin--extra');
     profileSection.classList.toggle('about-section__profile--secondary');
+    profileContainer.classList.toggle('profile__element--hidden');
+    profileImage.classList.toggle('profile__element--hidden');
+    for (let i=0; i < secondaryElements.length; i++) {
+        secondaryElements[i].classList.toggle('profile__element--hidden');
+    }
 })
 
