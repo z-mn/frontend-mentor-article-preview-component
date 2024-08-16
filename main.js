35ad677108569f75c7f2bd93f21a9c0a4b1ab71e
z-mn <155245405+z-mn@users.noreply.github.com>
2024-08-16 13:08:59 +0100

    minor css changes

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 
 const shareButton = document.getElementById('button');
 const shareImage = document.getElementById('image');
+const profileSection = document.getElementById('profile')
 
 shareButton.addEventListener('mouseover', () => {
     shareImage.style = 'fill: #FFFFFF; transition: 200ms fill ease-in';
@@ -9,5 +10,14 @@ shareButton.addEventListener('mouseover', () => {
 })
 
 shareButton.addEventListener('mouseleave', () => {
-    shareImage.style = 'fill: #6E8098; transition: 200ms fill ease-out';
+    shareImage.style = 'fill: #6E8098; transition: 200ms fill ease-out'; // this is stopping the button click image colour change.
 })
+
+// Button click - UI change
+
+shareButton.addEventListener('click', () => {
+    shareButton.classList.toggle('about-section__swap-button--secondary');
+    shareImage.classList.toggle('image--secondary');
+    profileSection.classList.toggle('about-section__profile--secondary');
+})
+
